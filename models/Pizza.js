3aabcdf48afe579f28e92b598124da67d484d0de
powerar <arpower@bell.net>
2021-06-16 19:02:27 -0400

    added data validation to comment, pizza, and reply models

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,9 +5,13 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      require: true,
+      trim: true
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -17,7 +21,9 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
-      default: 'Large',
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+      default: 'Large'
     },
     toppings: [],
     comments: [
